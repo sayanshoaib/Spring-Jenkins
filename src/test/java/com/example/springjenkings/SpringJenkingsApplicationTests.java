335e558bbabce2a99f2781bf3fcd902d6ceeefd3
shoaib <Shoaib.Hasan1801@gmail.com>
2022-06-30 06:16:46 -0700

    second commit, change in test contexloads

--- src/test/java/com/example/springjenkings/SpringJenkingsApplicationTests.java
+++ src/test/java/com/example/springjenkings/SpringJenkingsApplicationTests.java
@@ -19,6 +19,7 @@ class SpringJenkingsApplicationTests {
     @Test
     void contextLoads() {
         logger.info("Test case executing...");
+        logger.info("Test case executing second log statement");
         assertEquals(true, true);
     }
 
